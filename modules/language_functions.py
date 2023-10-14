3e48fac7854581356ce83dcec7812dbf8f3c04d1
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2023-10-14 16:02:43 +0400

    fix: fix language preference retrieval

--- modules/language_functions.py
+++ modules/language_functions.py
@@ -5,26 +5,36 @@ import os
 from telegram import Update
 from telegram.ext import ContextTypes
 
-# Get the directory where this script is located
+# get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Construct the path to the languages directory relative to the script's location
+# construct the path to the languages directory relative to the script's location
 languages_dir = os.path.join(script_dir, "..", "languages")
 
 
-# Define a function to get the user's language preference and the language file path
+# define a function to get the user's language preference and the language file path
 async def get_language(update: Update, context: ContextTypes.DEFAULT_TYPE) -> tuple:
-    # Define a dictionary that maps Telegram's language codes to your supported languages
+    # define a dictionary that maps Telegram's language codes to your supported languages
     supported_languages = {"en": "en", "ru": "ru", "fr": "fr", "es": "es", "de": "de"}
 
-    # Detect the user's language preference and set the default to English if not supported
-    user_language_code = update.message.from_user.language_code
+    # ensure update.message is not None and has from_user with a language_code
+    if (
+        update.message
+        and update.message.from_user
+        and hasattr(update.message.from_user, "language_code")
+    ):
+        user_language_code = update.message.from_user.language_code
+    else:
+        # Use a default language code (e.g., "en") if not available
+        user_language_code = "en"
+
+    # determine the user's language based on the code
     language = supported_languages.get(user_language_code, "en")
 
-    # Save the user's language preference in the user_data dictionary
+    # save the user's language preference in the user_data dictionary
     context.user_data["language"] = language
 
-    # Construct the language file path based on the user's language preference
+    # construct the language file path based on the user's language preference
     language_file_path = os.path.join(languages_dir, f"{language}_strings.json")
 
     return language, language_file_path
